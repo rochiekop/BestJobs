8237d4a1b0633ee321eb3e57b14e318f9c55cd4d
Rochi Eko Pambudi <rochieko11@gmail.com>
2020-12-10 20:53:32 +0700

    first init

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+import LoginPage from './component/LoginPage';
+const App = () => {
+  return (
+    <View>
+      <LoginPage />
+    </View>
+  );
+};
+export default App;
